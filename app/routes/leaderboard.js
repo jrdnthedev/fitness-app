419b7b1018ce794f3e57b30663186967935c5eca
smooooky <75350537+smooooky@users.noreply.github.com>
2022-01-02 23:32:27 -0500

    refactored route code

--- app/routes/leaderboard.js
+++ app/routes/leaderboard.js
@@ -1,13 +1,14 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
-import { hash } from 'rsvp';
 
 export default class LeaderboardRoute extends Route {
   @service store;
   async model() {
-    return hash({
-      team: this.store.findAll('team'),
-      contestant: this.store.findAll('contestant'),
-    });
+    let [team, contestant] = await Promise.all([
+      this.store.findAll('team'),
+      this.store.findAll('contestant'),
+    ]);
+
+    return { team, contestant };
   }
 }
