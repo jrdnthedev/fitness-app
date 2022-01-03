6b1b088f641a8c7554e64ce9cfac43d6e5a5ae46
smooooky <75350537+smooooky@users.noreply.github.com>
2022-01-02 20:46:41 -0500

    added table component,created new attr for model, added new fields to teams.json

--- app/models/team.js
+++ app/models/team.js
@@ -3,4 +3,5 @@ import Model, { attr } from '@ember-data/model';
 export default class TeamModel extends Model {
   @attr name;
   @attr members;
+  @attr scores;
 }
--- app/routes/leaderboard.js
+++ app/routes/leaderboard.js
@@ -1,3 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class LeaderboardRoute extends Route {}
+export default class LeaderboardRoute extends Route {
+  @service store;
+  async model() {
+    return this.store.findAll('team');
+  }
+}
