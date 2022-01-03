c33dc4997902444ce9f2e6ba33b8f8867eb14f2c
smooooky <75350537+smooooky@users.noreply.github.com>
2022-01-02 23:19:13 -0500

    added new models and components

--- app/routes/leaderboard.js
+++ app/routes/leaderboard.js
@@ -1,9 +1,13 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
 export default class LeaderboardRoute extends Route {
   @service store;
   async model() {
-    return this.store.findAll('team');
+    return hash({
+      team: this.store.findAll('team'),
+      contestant: this.store.findAll('contestant'),
+    });
   }
 }
--- tests/integration/components/teamtable-test.js
+++ tests/integration/components/teamtable-test.js
@@ -3,22 +3,22 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | table', function (hooks) {
+module('Integration | Component | teamtable', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.set('myAction', function(val) { ... });
 
-    await render(hbs`<Table />`);
+    await render(hbs`<Teamtable />`);
 
     assert.dom(this.element).hasText('');
 
     // Template block usage:
     await render(hbs`
-      <Table>
+      <Teamtable>
         template block text
-      </Table>
+      </Teamtable>
     `);
 
     assert.dom(this.element).hasText('template block text');
