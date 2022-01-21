6326181e4a6456b93bb0b28fcf7daecdc91edac4
smooooky <75350537+smooooky@users.noreply.github.com>
2022-01-21 00:09:20 -0500

    updated styles

--- app/components/chart.js
+++ app/components/chart.js
@@ -3,41 +3,42 @@ import Component from '@glimmer/component';
 export default class ChartComponent extends Component {
   chartOptions = {
     chart: {
-      type: 'area',
+      type: 'line',
+      style: {
+        fontFamily: 'sans-serif',
+        width: '100%',
+      },
     },
     title: {
       text: 'Fitness Chart',
     },
     xAxis: {
-      type: 'datetime',
+      type: 'categories',
+      categories: ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
     },
     yAxis: {
       title: {
-        text: 'Miles Ran',
+        text: 'Total',
       },
     },
   };
 
   chartData = [
     {
-      name: 'Tokyo',
-      data: [
-        7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6,
-      ],
+      name: 'Push Ups',
+      data: [104, 110, 100, 105, 138, 167, 200],
     },
     {
-      name: 'New York',
-      data: [
-        -0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5,
-      ],
+      name: 'Flower',
+      data: [66, 25, 55, 105, 38, 76, 43],
     },
     {
-      name: 'Berlin',
-      data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0],
+      name: 'Dips',
+      data: [86, 65, 50, 105, 120, 107, 30],
     },
     {
-      name: 'London',
-      data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8],
+      name: 'burpees',
+      data: [50, 70, 70, 85, 98, 97, 40],
     },
   ];
 }
