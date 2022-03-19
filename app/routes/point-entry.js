2c6289d103875e551f6743a0399df6eedd61f1aa
smooooky <75350537+smooooky@users.noreply.github.com>
2022-03-19 18:50:03 -0400

    added entry box component and updated tables styles

--- app/routes/point-entry.js
+++ app/routes/point-entry.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
 
-export default class PointEntryRoute extends Route {}
+export default class PointEntryRoute extends Route {
+  model() {
+    return [
+      { name: 'Monday' },
+      { name: 'Tuesday' },
+      { name: 'Wedensday' },
+      { name: 'Thursday' },
+      { name: 'Friday' },
+      { name: 'Saturday' },
+      { name: 'Sunday' },
+    ];
+  }
+}
