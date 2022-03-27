d47eb8a66406d8267ae564642aa885ba09d7954a
smooooky <75350537+smooooky@users.noreply.github.com>
2022-03-26 22:31:37 -0400

    updated leaderboard template and related component code

--- app/routes/leaderboard.js
+++ app/routes/leaderboard.js
@@ -4,11 +4,6 @@ import { inject as service } from '@ember/service';
 export default class LeaderboardRoute extends Route {
   @service store;
   async model() {
-    let [team, contestant] = await Promise.all([
-      this.store.findAll('team'),
-      this.store.findAll('contestant'),
-    ]);
-
-    return { team, contestant };
+    return this.store.findAll('team', { include: 'user' });
   }
 }
